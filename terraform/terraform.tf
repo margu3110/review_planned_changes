23a6490e88266bfd03b147aa60a0b6ac20626daf
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-04-03 09:08:41 -0300

    troubleshooting terraform backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-backend-atlas-test-00001"
-    key            = "atlas-test.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "tf-state-backend-atlas-test-00001"
+#     key     = "atlas-test.tfstate"
+#     region  = "us-east-1"
+#     encrypt = true
+#   }
+# }
 
 terraform {
   required_version = "~> 1.5"
@@ -13,19 +13,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.14"
+      version = "~> 5.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "terraform-isildur"
   default_tags {
     tags = {
-      component  = "devops"
-      created-by = "terraform"
+      component   = "devops"
+      created-by  = "terraform"
       environment = "test"
       project     = "atlas-test"
     }
