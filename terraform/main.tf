1760a03239d77774ce94d85a288784ac256cdbfe
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-04-03 08:29:48 -0300

    testing the github workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "atlas-test-bucket-000001"
+resource "aws_s3_bucket" "atlas-test-bucket-000002" {
+  bucket = "atlas-test-bucket-000002"
 
   tags = {
-    Name = "atlas-test-bucket-000001"
+    Name = "atlas-test-bucket-000002"
   }
 }
